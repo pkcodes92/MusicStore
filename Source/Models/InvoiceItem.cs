4af7268377b36a6818bf1c5264c6069c45eb48a9
pkcodes92 <pkrish08@gmail.com>
2020-11-21 17:08:16 -0500

    Having the necessary changes made for the models - with the Invoice Item and Employee models added now

---
+++ Source/Models/InvoiceItem.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="InvoiceItem.cs" company="PK Codes">
+// Copyright (c) PK Codes. All rights reserved.
+// </copyright>
+
+namespace MusicStore.Models
+{
+    /// <summary>
+    /// This class models an invoice line item.
+    /// </summary>
+    public class InvoiceItem
+    {
+    }
+}
