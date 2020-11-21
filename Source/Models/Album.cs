b9a547a7f5d9adfd84090de19952806dff00f7c1
pkcodes92 <pkrish08@gmail.com>
2020-11-20 21:04:02 -0500

    Adding in the necessary classes and also the right NuGet packages accordingly

---
+++ Source/Models/Album.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Album.cs" company="PK Codes">
+// Copyright (c) PK Codes. All rights reserved.
+// </copyright>
+
+namespace MusicStore.Models
+{
+    /// <summary>
+    /// This class models an album.
+    /// </summary>
+    public class Album
+    {
+        /// <summary>
+        /// Gets or sets the album ID.
+        /// </summary>
+        public int AlbumId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the album.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the artist ID.
+        /// </summary>
+        public int ArtistId { get; set; }
+    }
+}
