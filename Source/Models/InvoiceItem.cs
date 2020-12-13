a4468eb62ed36fba9e637ba475b5459838c2ac51
pkcodes92 <pkrish19@outlook.com>
2020-12-13 12:28:40 -0500

    Adding the necessary migrations

--- Source/Models/InvoiceItem.cs
+++ Source/Models/InvoiceItem.cs
@@ -4,10 +4,37 @@
 
 namespace MusicStore.Models
 {
+    using Microsoft.EntityFrameworkCore;
+
     /// <summary>
     /// This class models an invoice line item.
     /// </summary>
+    [Keyless]
     public class InvoiceItem
     {
+        /// <summary>
+        /// Gets or sets the invoice line ID.
+        /// </summary>
+        public int InvoiceLineId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the invoice ID.
+        /// </summary>
+        public int InvoiceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the track ID.
+        /// </summary>
+        public int TrackId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unit price.
+        /// </summary>
+        public string UnitPrice { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quantity.
+        /// </summary>
+        public int Quantity { get; set; }
     }
 }
