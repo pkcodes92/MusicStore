daeb7c8a5ce6fa960d297d27d652f1959eaab7c5
pkcodes92 <pkrish19@outlook.com>
2021-01-15 21:18:51 -0500

    Adding the tracks and customers controller classes.

---
+++ Source/Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="CustomersController.cs" company="PK Codes">
+// Copyright (c) PK Codes. All rights reserved.
+// </copyright>
+
+namespace MusicStore.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// This is the customers controller.
+    /// </summary>
+    [Route("api/Customers")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // GET: api/<CustomersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CustomersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Source/Controllers/TracksController.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="TracksController.cs" company="PK Codes">
+// Copyright (c) PK Codes. All rights reserved.
+// </copyright>
+
+namespace MusicStore.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using MusicStore;
+    using MusicStore.Models;
+
+    [Route("api/Tracks")]
+    [ApiController]
+    public class TracksController : ControllerBase
+    {
+        private readonly MusicStoreDbContext context;
+
+        public TracksController(MusicStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Track>>> GetTracks()
+        {
+            return await this.context.Tracks.ToListAsync().ConfigureAwait(false);
+        }
+    }
+}
