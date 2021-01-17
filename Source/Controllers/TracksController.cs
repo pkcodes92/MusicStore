b66e640c20c1a9025c80dcfd270992c01f4ce4a7
pkcodes92 <pkrish19@outlook.com>
2021-01-16 22:55:55 -0500

    Additional changes being made

--- Source/Controllers/TracksController.cs
+++ Source/Controllers/TracksController.cs
@@ -4,7 +4,9 @@
 
 namespace MusicStore.Controllers
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
@@ -55,6 +57,51 @@ namespace MusicStore.Controllers
             }
 
             return track;
+        }
+
+        /// <summary>
+        /// This method will update the track information.
+        /// </summary>
+        /// <param name="id">The track ID.</param>
+        /// <param name="track">The track information to update.</param>
+        /// <returns>A unit of execution that will contain a type of <see cref="IActionResult"/>.</returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTrack(int id, Track track)
+        {
+            if (track is null)
+            {
+                throw new ArgumentNullException(nameof(track));
+            }
+
+            if (id != track.TrackId)
+            {
+                return this.BadRequest();
+            }
+
+            this.context.Entry(track).State = EntityState.Modified;
+
+            try
+            {
+                await this.context.SaveChangesAsync().ConfigureAwait(false);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!this.TrackExists(id))
+                {
+                    return this.NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return this.NoContent();
+        }
+
+        private bool TrackExists(int id)
+        {
+            return this.context.Tracks.Any(e => e.TrackId == id);
         }
     }
 }
